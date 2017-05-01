b85eed8d9e3650f2f1cf117064826a786a2069f2
hyungsun.git <winter765p@gmail.com>
2017-05-01 12:52:35 +0900

    0501_1차 반응형 작업 HTML/CSS 수정

--- scripts/pc.js
+++ scripts/pc.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $(".list_sub").hide();
     
+    /* 화면 최상위 일때 header */
     $('body').scrollTop(function(){
         if ($('body').scrollTop() == 0){
                 var $mainH = $('#zionHead');
@@ -19,6 +20,7 @@ $(document).ready(function () {
         }
     });
     
+    /* drop menu 충돌 방지 */
     var downBtn = $(".link_gnb2"),
                 prevBtn = $(".link_gnb1"),
                 nextBtn = $(".link_gnb3");
@@ -39,6 +41,7 @@ $(document).ready(function () {
                 $(".list_sub").slideUp(300).stop().hide();
             });
     
+    /* 스크롤 시 변하는 메뉴 */
     $(window).scroll(function () {
         
         var $thisW = $(this),
@@ -54,19 +57,15 @@ $(document).ready(function () {
             $head.addClass('head_type1');
         }
     });
+    
+    /* MOBILE 메뉴 토글 옵션 */
+    $("#toggle").click(function() {
+        $(this).toggleClass("on");
+        $(".drop_toggle").slideToggle();
+    });
+    
+    /* MOBILE SUB 토글 옵션 */
+    $(".open_toggle").click(function() {
+       $(".sub_toggle").slideToggle(); 
+    });
 });
-
-
-
-
-//    var $mainH = $('#zionHead');
-//    
-//        $mainH.on("mouseenter",function(){
-//            $mainH.removeClass('head_type1');
-//            $mainH.addClass('head_type2');
-//        });
-//    
-//            $mainH.on("mouseleave",function(){
-//            $mainH.removeClass('head_type2');
-//            $mainH.addClass('head_type1');
-//        });
